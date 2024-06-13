04c0a56066a407f4276d69d4e1a8abae05f12aa7
elilourenco <lourencomonica318@gmail.com>
2024-06-13 16:02:26 +0100

    header profile, header modular

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import Image from "next/image";
+import React, { useEffect } from "react";
+
+export default function Header(){
+    const [isScrolled, setIsScrolled] = React.useState(false);
+
+    useEffect(()=>{
+        const handleScroll =() =>{
+
+            if(window.scrollY > 0){
+                setIsScrolled(true)
+            }
+                else{
+                    setIsScrolled
+                }
+
+            
+            window.addEventListener('scroll', handleScroll);
+            return()=> window.removeEventListener('scroll',handleScroll)
+        }
+    },[])
+    return(
+        <header className={`${isScrolled && 'bg-black'}  fixed top-0z-50 flex w-full items-center justify-between 
+        transition-all px-4 py-4 lg:px-10 lg:py-6`}>
+
+            <div className="flex items-center space-x-2 md:space-x-8">
+
+                <img src='https://rb.gy/ulxxee' alt="logo of the page" width={120} height={120}/>
+
+                <ul className="hidden md:flex md:space-x-4">
+                    <li>Home</li>
+                    <li>Tv Shows</li>
+                    <li>Movie</li>
+                    <li>Latest</li>
+                </ul>
+            </div>
+            <div className="flex items-center space-x-4">
+                 <p className="hidden cursor-not-allowed lg:inline">Kids</p>
+                 <img src='https://rb.gy/g1pwyx' 
+                 alt=""
+                 className="cursor-pointer rounded" />
+            </div>
+        </header>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="relative h-full lg:h-[140vh]">
-      <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
+    <div className="relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]">
+      <Header />
+      <main className="relative pb-24 pl-4  lg:pl-16">
 
         <div className=" flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
           <div className=" absolute flex flex-col left-0 top-0 -z-10 h-[95vh] w-screen bg-black">
@@ -12,13 +14,10 @@ export default function Home() {
             src="/banner.png"
             alt=" the witcher"
             fill={true}
-            className="object-top h-[65vh] lg:h-[95vh]:"
+            className="object-cover  h-[65vh] lg:h-[95vh]:"
             />
           </div>
-            
-            
-            
-            <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+              <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
               The Witcher
             </h1>
             <p className="text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl">
@@ -39,7 +38,7 @@ export default function Home() {
          bg-white
          text-sm
          px-5 py-1.5
-          font-semibold
+         font-semibold
          text-black 
          transition hover:opacity-75 
          md:px-8
