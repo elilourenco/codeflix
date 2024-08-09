16b644bd57bd6ae7736143925f02fdeba1300fd6
elilourenco <lourencomonica318@gmail.com>
2024-08-09 19:29:55 +0100

    change the code , transformation of the header, create the compomentes, userprofile,logo,navlinks

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from "next/image";
 import React, { useEffect } from "react";
 
 export default function Header(){
@@ -14,20 +13,46 @@ export default function Header(){
             }
                 else{
                     setIsScrolled
-                }
-
-            
+                }    
+                
+                
             window.addEventListener('scroll', handleScroll);
-            return()=> window.removeEventListener('scroll',handleScroll)
+            return()=> {
+                window.removeEventListener('scroll',handleScroll)}
         }
     },[])
+
+   return isScrolled;
+
+   const Logo= ()=> (
+    <img src='http://rb.gy/ulxxee' alt="logo of the page" width={120} height={120}/>
+   )
+
+   const NavLinks =() => (
+    <nav>
+        <ul className="hidden md:flex md:space-x-4">
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movie</li>
+            <li>Latest</li>
+
+        </ul>
+
+    </nav>
+   )
+
+   const Userprofile =()=>(
+    <div>
+
+    </div>
+   )
     return(
         <header className={`${isScrolled && 'bg-black'}  fixed top-0z-50 flex w-full items-center justify-between 
         transition-all px-4 py-4 lg:px-10 lg:py-6`}>
 
             <div className="flex items-center space-x-2 md:space-x-8">
 
-                <img src='https://rb.gy/ulxxee' alt="logo of the page" width={120} height={120}/>
+
 
                 <ul className="hidden md:flex md:space-x-4">
                     <li>Home</li>
