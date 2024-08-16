28f0ccfce9313ba393684ef955889f8fa857b6db
elilourenco <lourencomonica318@gmail.com>
2024-08-16 08:44:26 +0100

    first archive of the test

---
+++ __tests__/hooks/useScroll.test.tsx
@@ -0,0 +1,26 @@
+import { useScroll } from "@/hooks/useScroll";
+import { describe } from "node:test";
+
+import {renderHook,act} from '@testing-library/react'
+
+describe('usescroll',()=> {
+    it('should response to scroll events ', ()=>{
+        const {result} = renderHook(()=> useScroll());
+
+     act(()=>{
+        global.window.scrollY = 100;
+        global.window.dispatchEvent(new Event('scroll'))
+    })
+
+    expect(result.current).toBe(true);
+
+
+    act(()=>{
+        global.window.scrollY=0;
+        global.window.dispatchEvent(new Event('scroll'))
+    })
+
+    expect(result.current).toBe(false)
+
+    });
+})
