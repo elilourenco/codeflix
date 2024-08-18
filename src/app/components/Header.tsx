6730908d8caffb98a0ac042177c410ad53280164
elilourenco <lourencomonica318@gmail.com>
2024-08-18 19:55:33 +0100

    add new fhotos

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,9 +10,8 @@ export default function Header(){
     const isScrolled= useScroll() ;
    
     return(
-        <header className={`${isScrolled && 'bg-black'}  
-        fixed top-0z-50 flex w-full items-center justify-between 
-        transition-all px-4 py-4 lg:px-10 lg:py-6`}>
+        <header className={`${isScrolled && 'bg-black'} fixed top-0z-50 flex w-full items-center
+         justify-between transition-all px-4 p-2 lg:px-16 lg:py-6`}>
 
             <div className="flex items-center space-x-2 md:space-x-8">
                 <Logo/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,10 +69,31 @@ export default function Home() {
         </button>
 
         </div>
+        
+      <div className="flex-col space-y-4">
+        <div className="flex">
+          <h2 className="-ml-2 inline-flex items-center text-2xl font-bold">Featured</h2>
+        </div>
+        <div className="-ml-8 flex space-x-4 overflow-x-scroll p-6 scrollbar-hide">
+            {[1,2,3,4,5,5,5,5].map((index) =>(
+              <div key={index} 
+              className=" group  h-28 min-w-[200px] relative
+               transition duration-200 ease-in transform  hover:scale-110 hover:z-50
+               md:h-40 md:min-w-[300px] lg:h-52 lg:min-w-[400px]
+                ">
+              <Image
+              
+              src={`/item_${index}.png`}
+              fill={true}
+              alt='MAID' 
+              className="rounded"         
+            />
+            </div>
+            ))}
 
-        
-       
-        
+          
+      </div>
+      </div>
         
       </main>
     </div>
