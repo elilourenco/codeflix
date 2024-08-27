2ab5e1c35336161e6982158b0769c99fbb86f766
elilourenco <lourencomonica318@gmail.com>
2024-08-27 09:25:17 +0100

    add the page of the not-found

---
+++ src/app/settings/user/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Notfound(){
+    return(
+        <div>
+            <h1>The user you are looking for is not found</h1>
+        </div>
+    )
+}
